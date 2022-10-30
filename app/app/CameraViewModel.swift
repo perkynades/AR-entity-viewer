2d1336abaaa440c4fac56a46d3d8278eec3d419d
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-10-30 13:42:36 +0100

    Finish camera view model

--- app/app/CameraViewModel.swift
+++ app/app/CameraViewModel.swift
@@ -57,7 +57,7 @@ class CameraViewModel: ObservableObject {
     var readyToCapture: Bool {
         captureFolderState != nil
                 && captureFolderState!.captures.count < CameraViewModel.maxPhotosAllowed
-                && self.inprogressPhotoCaptureDelegates.count < 2
+                && self.inProgressPhotoCaptureDelegates.count < 2
     }
 
     var captureDir: URL? {
@@ -91,7 +91,7 @@ class CameraViewModel: ObservableObject {
         case .manual:
             capturePhotoAndMetadata()
         case .automatic:
-            guard triggereveryTimer != nil else { return }
+            guard triggerEveryTimer != nil else { return }
             if triggerEveryTimer!.isRunning {
                 stopAutomaticCapture()
             } else {
@@ -152,8 +152,8 @@ class CameraViewModel: ObservableObject {
             self.configureSession()
         }
 
-        if motionmanager.isDeviceMotionAvailable {
-            motionManager.StartDeviceMotionUpdates()
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates()
             DispatchQueue.main.async {
                 self.isMotionDataEnabled = true
             }
@@ -234,6 +234,180 @@ class CameraViewModel: ObservableObject {
 
     private var photoOutput = AVCapturePhotoOutput()
     private var inProgressPhotoCaptureDelegates = [Int64: PhotoCaptureProcessor]()
+    private var triggerEveryTimer: TriggerEveryTimer? = nil
+
+    private func capturePhotoAndMetadata() {
+        dispatchPrecondition(condition: .onQueue(.main))
+
+        let videoPreviewLayerOrientation = session.connections[0].videoOrientation
+
+        sessionQueue.async {
+            if let photoOutputConnection = self.photoOutput.connection(with: .video) {
+                photoOutputConnection.videoOrientation = videoPreviewLayerOrientation
+            }
+
+            var photoSettings = AVCapturePhotoSettings()
+
+            if self.photoOutput.availablePhotoCodecTypes.contains(.hevc) {
+                photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
+            }
+
+            if self.videoDeviceInput!.device.isFlashAvailable {
+                photoSettings.flashMode = .off
+            }
+
+            photoSettings.isHighResolutionPhotoEnabled = true
+            photoSettings.isDepthDataDeliveryEnabled = self.photoOutput.isDepthDataDeliveryEnabled
+            photoSettings.photoQualityPrioritization = self.photoQualityPrioritizationMode
+            photoSettings.embedsDepthDataInPhoto = true
+
+            if !photoSettings.__availablePreviewPhotoPixelFormatTypes.isEmpty {
+                photoSettings.previewPhotoFormat = [
+                    kCVPixelBufferPixelFormatTypeKey: photoSettings.__availablePreviewPhotoPixelFormatTypes.first!,
+                    kCVPixelBufferWidthKey: self.previewWidth,
+                    kCVPixelBufferHeightKey: self.previewHeight
+                ] as [String: Any]
+                print("Found preview photo")
+            } else {
+                print("Cant find preview photo")
+            }
+
+            photoSettings.embeddedThumbnailPhotoFormat = [
+                AVVideoCodecKey: AVVideoCodecType.jpeg,
+                AVVideoWidthKey: self.thumbnailWidth,
+                AVVideoHeightKey: self.thumbnailHeight
+            ]
+
+            DispatchQueue.main.async {
+                self.isHighQualityMode =
+                        photoSettings.isHighResolutionPhotoEnabled
+                        && photoSettings.photoQualityPrioritization == .quality
+            }
+
+            self.photoId += 1
+
+            let photoCaptureProcessor = self.makeNewPhotoCaptureProcessor(photoId: self.photoId, photoSettings: photoSettings)
+            self.inProgressPhotoCaptureDelegates[photoCaptureProcessor.requestPhotoSettings.uniqueID] = photoCaptureProcessor
+
+            self.photoOutput.capturePhoto(with: photoSettings, delegate: photoCaptureProcessor)
+        }
+    }
+
+    private func makeNewPhotoCaptureProcessor(photoId: UInt32, photoSettings: AVCapturePhotoSettings) -> PhotoCaptureProcessor {
+        let photoCaptureProcessor = PhotoCaptureProcessor(
+                with: photoSettings,
+                model: self,
+                photoId: photoId,
+                motionManager: self.motionManager,
+                willCapturePhotoAnimation: {
+                    AudioServicesPlaySystemSound(CameraViewModel.cameraShutterNoiseID)
+                },
+                completionHandler: { photoCaptureProcessor in
+                    self.sessionQueue.async {
+                        self.inProgressPhotoCaptureDelegates.removeValue(forKey: photoCaptureProcessor.requestPhotoSettings.uniqueID)
+                    }
+                }, photoProcessingHandler: { _ in  }
+        )
+        return photoCaptureProcessor
+    }
+
+    private func startAutomaticCapture() {
+        dispatchPrecondition(condition: .onQueue(.main))
+        precondition(triggerEveryTimer != nil)
+
+        guard !triggerEveryTimer!.isRunning else {
+            return
+        }
+        triggerEveryTimer!.start()
+
+        isAutoCaptureActive = true
+    }
+
+    private func stopAutomaticCapture() {
+        dispatchPrecondition(condition: .onQueue(.main))
+
+        isAutoCaptureActive = false
+        triggerEveryTimer?.stop()
+    }
+
+    private static func createNewCaptureFolder() throws -> CaptureFolderState {
+        guard let newCaptureDir = CaptureFolderState.createCaptureDirectory() else {
+            throw SetupError.failed(msg: "Cant create capture directory")
+        }
+        return CaptureFolderState(url: newCaptureDir)
+    }
+
+    private func requestAuthorizationIfNeeded() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            break
+        case .notDetermined:
+            sessionQueue.suspend()
+            AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+                if !granted {
+                    self.setupResult = .notAuthorized
+                }
+                self.sessionQueue.resume()
+            })
+        default:
+            setupResult = .notAuthorized
+        }
+    }
+
+    private func configureSession() {
+        guard setupResult == .inProgress else {
+            return
+        }
+
+        session.beginConfiguration()
+        defer { session.commitConfiguration() }
+
+        session.sessionPreset = .photo
+
+        do {
+            let videoDeviceInput = try AVCaptureDeviceInput(device: getVideoDeviceForPhotogrammetry())
+
+            if session.canAddInput(videoDeviceInput) {
+                session.addInput(videoDeviceInput)
+                self.videoDeviceInput = videoDeviceInput
+            } else {
+                setupResult = .configurationFailed
+                return
+            }
+        } catch {
+            print("Could not create video device input")
+            setupResult = .configurationFailed
+            return
+        }
+
+        DispatchQueue.main.async {
+            self.isDepthDataEnabled = self.photoOutput.isDepthDataDeliveryEnabled
+            self.isHighQualityMode =
+                    self.photoOutput.isHighResolutionCaptureEnabled
+                    && self.photoOutput.maxPhotoQualityPrioritization == .quality
+        }
+
+        setupResult = .success
+    }
+
+    private func getVideoDeviceForPhotogrammetry() throws -> AVCaptureDevice {
+        var defaultVideoDevice: AVCaptureDevice?
+
+        if let dualCameraDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+            defaultVideoDevice = dualCameraDevice
+        } else if let dualWideCameraDevice = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: .back) {
+            defaultVideoDevice = dualWideCameraDevice
+        } else if let backWideCameraDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            defaultVideoDevice = backWideCameraDevice
+        }
+
+        guard let videoDevice = defaultVideoDevice else {
+            print("Back video device unavailable.")
+            throw SessionSetupError.configurationFailed
+        }
+
+        return videoDevice
+    }
 }
 
 
